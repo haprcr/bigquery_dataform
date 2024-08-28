dd941a6eb41cee631039a4e9bafd9846a1deccd0
haprcr <124026578+haprcr@users.noreply.github.com>
2024-08-28 14:54:41 +0530

    Create create.sql

---
+++ create.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `indigo-medium-362704.1_29feb_resource_cud.metric_2`
+(
+  Usage_Date DATE,
+  Project_Id STRING,
+  Project_Name STRING,
+  Region STRING,
+  Service STRING,
+  SKU_Id STRING,
+  SKU_Description STRING,
+  Usage_Type STRING,
+  CUD_Type STRING,
+  Unit_Type STRING,
+  CUD_Credit_Usage_Amount FLOAT64,
+  Resource_eligible_Usage_Amount FLOAT64,
+  CUD_Credit FLOAT64,
+  Resource_Cost FLOAT64,
+  `Cud_Coverage%` FLOAT64
+);
